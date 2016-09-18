909b763b1effd4a35eafcca028e5231ee297ca62
Ahmed Elnoamany <ahmedgouda9484@gmail.com>
2016-09-17 22:40:58 -0500

    Created main.js and prototypes.js. Added some prototypes.

---
+++ prototypes.js
@@ -0,0 +1,18 @@
+function Beast(name, health, beastType, attacks, owner){  //beasts
+  this.name = name;
+  this.health = health;
+  this.beastType = beastType;
+  this.attacks = attacks;
+  this.owner = owner;
+}
+function Attack(name, damageInflicted, beastType){  //beast attacks
+  this.name = name;
+  this.damageInflicted = damageInflicted;
+  this.beastType = beastType;
+}
+function Player(name, health){  //main player
+
+}
+function Bag(){   //bag of items
+
+}
